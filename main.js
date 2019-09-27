a93f62dc761619f2349bf2147fc0ebf1844fd8a3
Shayhaque <shayhaq85@gmail.com>
2019-09-27 14:00:28 +0500

    added maps in footer

--- main.js
+++ main.js
@@ -1,33 +1,5 @@
 
 
-/*var slider_content = document.querySelector('#box');
-  var image = ['a', 'b','c'];
-  var i = image.length;
-  function nextImage(){
-    if (i<image.length) {
-      
-      i = i+1;
-    }else
-    {
-      i = 1;
-    }
-    slider_content.innerHTML = "<img src ="+image[i-1]+".jpg>";
-    clearInterval(nextImage);
-    
-  }
-  setInterval(nextImage, 3000);
-  function prewImage (){
-   
-    if (i<image.length+1 && i>1){
-      i = i-1;
-    }else
-    {
-      i = image.length;
-    }
-    slider_content.innerHTML = "<img src ="+image[i-1]+".jpg>";
-    clearInterval(nextImage);
-  }*/
-
   /*hamburgor menu*/
   let togglenavabr = false;
 
